354e3cd50f4911c75fa560d695b048a4fd7166fe
Samarth <sanjeevpkadakol1@gmail.com>
2026-01-13 20:03:57 +0530

    Bind server to 0.0.0.0 to fix Replit proxy connection

--- server/index.js
+++ server/index.js
@@ -73,7 +73,7 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
 }
